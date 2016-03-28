9174a3ed0620ed83bbbcc4515b147acf7ee1dc3e
Jonathan <jonacom@lissismore.com>
2016-03-28 14:38:40 -0600

    removed comments from main:

--- main.cpp
+++ main.cpp
@@ -29,25 +29,25 @@ int main()
   list.addNode("eighth node");
   list.printList();
 
-  // cout << endl << "looking for fourth node:" << endl;
-  // cout << "found: " << list.find("fourth node")->data << endl << endl;
-  // cout << endl << "looking for invalid node:" << endl;
-  // if (list.find("slkdjf") == nullptr)
-  //   cout << "found nullptr" << endl << endl;
-  //
-  // cout << "deleting third node..." << endl;
-  // list.deleteNode("third node");
-  // list.printList();
-  //
-  // cout << endl << "finding parent of fifth node" << endl;
-  // cout << "found: " << list.findParent("fifth node")->data << endl;
-  //
-  // cout << endl << "Inserting 'new node' after 'first node'" << endl << endl;
-  // list.insertAfter("new node", "first node");
-  // list.printList();
-  //
-  // cout << endl << "Trying to insert 'new node' after invalid node 'asdf'" << endl;
-  // list.insertAfter("new node", "asdf");
+  cout << endl << "looking for fourth node:" << endl;
+  cout << "found: " << list.find("fourth node")->data << endl << endl;
+  cout << endl << "looking for invalid node:" << endl;
+  if (list.find("slkdjf") == nullptr)
+    cout << "found nullptr" << endl << endl;
+
+  cout << "deleting third node..." << endl;
+  list.deleteNode("third node");
+  list.printList();
+
+  cout << endl << "finding parent of fifth node" << endl;
+  cout << "found: " << list.findParent("fifth node")->data << endl;
+
+  cout << endl << "Inserting 'new node' after 'first node'" << endl << endl;
+  list.insertAfter("new node", "first node");
+  list.printList();
+
+  cout << endl << "Trying to insert 'new node' after invalid node 'asdf'" << endl;
+  list.insertAfter("new node", "asdf");
 
   cout << endl << "Insert after last node (AKA append)" << endl;
   list.insertAfter("new node", "eighth node");
